709538e9a567e19b610b1a5bbe0c647a00e69289
unknown <saeid.hatamikia67@gmail.com>
2020-08-01 23:54:19 +0430

    add aray and obj

---
+++ Obj_Array_Practice.js
@@ -0,0 +1,13 @@
+let arar =[20,30,45];
+
+let obj={name:"saeed",family:"alavi",
+start:function(){
+    console.log("test func in obj");
+}
+}
+
+let arrayobj=[
+{name:"saeed",family:"hatamikai"}
+
+
+]
