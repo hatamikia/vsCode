dc1734dbc83e690dcc0c363d1d29b3cec6fc9e8a
unknown <saeid.hatamikia67@gmail.com>
2020-08-02 14:14:39 +0430

    practice learning

--- Obj_Array_Practice.js
+++ Obj_Array_Practice.js
@@ -1,13 +1,43 @@
-let arar =[20,30,45];
+let arar = [20, 30, 45];
 
-let obj={name:"saeed",family:"alavi",
-start:function(){
-    console.log("test func in obj");
-}
+let obj = {
+    name: "saeed", family: "alavi",
+    start: function () {
+        console.log("test func in obj");
+    }
 }
 
-let arrayobj=[
-{name:"saeed",family:"hatamikai"}
+let arrayobj = [
+    { name: "saeed", family: "hatamikai" }
 
 
 ]
+
+//bedone mghdare avvaliye meghadr undefind ast
+var name;
+// if prin now you see undefind 
+console.log(name);
+name = "saeed";
+//if now print saeed value
+//2 hite dar javasacript (global scope) and  (function scope)
+
+function getDate() {
+    var date = new Date()
+    let date2 = new Date()
+    const date3 = new Date()
+    return date
+}
+
+getDate()
+console.log(date)  //❌ Reference Error
+console.log(date2) //❌ Reference Error
+console.log(date3) //❌ Reference Error
+
+var mm = 21;  //  tabei
+let nn = 42;    // blocki
+const kk = 54; // read only
+
+
+var araye =[1,2,3]
+var obj ={key:"value"}
+var arrayobj=[{key:'value'},{key:"value"}]
